82ce320a144fa82d4016007c006c6937348464cc
lovrodijanovic <lovrodijanovic98@gmail.com>
2023-12-14 17:42:29 +0100

    Created basic app layout

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import {
+  SharedModule,
+  FooterComponent,
+  HeaderComponent,
+} from './shared';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     AppRoutingModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
