a027dbc313b06b75618de8cc93ae679433f94b9b
Zokky2e <icro4.gaming@hotmail.com>
2023-12-27 16:24:23 +0100

    added details page and route

--- ClientApp/src/app/events/events.module.ts
+++ ClientApp/src/app/events/events.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { EventsRoutingModule } from './events-routing.module';
-import { HomeComponent } from './home/home.component';
+import { EventsRoutingModule } from "./events-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { DetailsPageComponent } from "./details-page/details-page.component copy";
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, EventsRoutingModule],
+	declarations: [HomeComponent, DetailsPageComponent],
+	imports: [CommonModule, EventsRoutingModule],
 })
 export class EventsModule {}
