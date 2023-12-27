2c2e1a189e37fac047d5ba836929d05b07c66158
Zokky2e <icro4.gaming@hotmail.com>
2023-12-27 16:24:13 +0100

    prettier reformatting files

--- ClientApp/src/app/models/DTOs/EventDTO.ts
+++ ClientApp/src/app/models/DTOs/EventDTO.ts
@@ -1,33 +1,36 @@
 export class EventDTO {
-  teamOneName: string;
-  teamTwoName: string;
-  teamOneScore: number;
-  teamTwoScore: number;
-  shortDescription: string;
-  description: string;
-  date: Date;
-  photoUrls: string[];
-  categoryNames: string[];
+	id: string;
+	teamOneName: string;
+	teamTwoName: string;
+	teamOneScore: number;
+	teamTwoScore: number;
+	shortDescription: string;
+	description: string;
+	date: Date;
+	photoUrls: string[];
+	categoryNames: string[];
 
-  constructor(
-    teamOneName: string,
-    teamTwoName: string,
-    teamOneScore: number,
-    teamTwoScore: number,
-    shortDescription: string,
-    description: string,
-    date: Date,
-    photoUrls: string[],
-    categoryNames: string[]
-  ) {
-    this.teamOneName = teamOneName;
-    this.teamTwoName = teamTwoName;
-    this.teamOneScore = teamOneScore;
-    this.teamTwoScore = teamTwoScore;
-    this.shortDescription = shortDescription;
-    this.description = description;
-    this.date = date;
-    this.photoUrls = photoUrls;
-    this.categoryNames = categoryNames;
-  }
+	constructor(
+		id: string,
+		teamOneName: string,
+		teamTwoName: string,
+		teamOneScore: number,
+		teamTwoScore: number,
+		shortDescription: string,
+		description: string,
+		date: Date,
+		photoUrls: string[],
+		categoryNames: string[]
+	) {
+		this.id = id;
+		this.teamOneName = teamOneName;
+		this.teamTwoName = teamTwoName;
+		this.teamOneScore = teamOneScore;
+		this.teamTwoScore = teamTwoScore;
+		this.shortDescription = shortDescription;
+		this.description = description;
+		this.date = date;
+		this.photoUrls = photoUrls;
+		this.categoryNames = categoryNames;
+	}
 }
--- ClientApp/src/app/services/event.service.ts
+++ ClientApp/src/app/services/event.service.ts
@@ -1,18 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { EventDTO } from '../models/DTOs/EventDTO';
+import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { EventDTO } from "../models/DTOs/EventDTO";
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: "root",
 })
 export class EventService {
-  constructor(
-    public http: HttpClient,
-    @Inject('BASE_URL') public baseUrl: string
-  ) {}
+	constructor(
+		public http: HttpClient,
+		@Inject("BASE_URL") public baseUrl: string
+	) {}
 
-  public getAllEvents(): Observable<EventDTO[]> {
-    return this.http.get<EventDTO[]>(this.baseUrl + 'event/list');
-  }
+	public getAllEvents(): Observable<EventDTO[]> {
+		return this.http.get<EventDTO[]>(this.baseUrl + "event/list");
+	}
+
+	public getEventById(eventId: string): Observable<EventDTO> {
+		return this.http.get<EventDTO>(this.baseUrl + `event/${eventId}`);
+	}
 }
