e206ecb633e9e3ec36ed0ff17bfb26796d67f405
Zokky2e <icro4.gaming@hotmail.com>
2023-12-28 17:40:09 +0100

    added admin header

--- ClientApp/src/app/administration/administration.module.ts
+++ ClientApp/src/app/administration/administration.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AdministrationRoutingModule } from './administration-routing.module';
-import { LoginComponent } from './login/login.component';
-import { SharedModule } from '../shared';
-import { AdminPageComponent } from './admin-page/admin-page.component';
+import { AdministrationRoutingModule } from "./administration-routing.module";
+import { LoginComponent } from "./login/login.component";
+import { SharedModule } from "../shared";
+import { AdminPageComponent } from "./admin-page";
+import { AdminHeaderComponent } from "./admin-header";
 
 @NgModule({
-  declarations: [LoginComponent, AdminPageComponent],
-  imports: [CommonModule, AdministrationRoutingModule, SharedModule],
+	declarations: [LoginComponent, AdminPageComponent, AdminHeaderComponent],
+	imports: [CommonModule, AdministrationRoutingModule, SharedModule],
 })
 export class AdministrationModule {}
