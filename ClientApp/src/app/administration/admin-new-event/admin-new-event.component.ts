a67cc241f3b49a32468393ff93efe47bdd56c134
Zokky2e <icro4.gaming@hotmail.com>
2023-12-28 18:57:35 +0100

    added simple form for entering new event - currently no logic

---
+++ ClientApp/src/app/administration/admin-new-event/admin-new-event.component.ts
@@ -0,0 +1,8 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+
+@Component({
+	selector: "admin-new-event",
+	templateUrl: "./admin-new-event.component.html",
+	styleUrls: ["./admin-new-event.component.css"],
+})
+export class AdminNewEventComponent {}
--- ClientApp/src/app/administration/administration.module.ts
+++ ClientApp/src/app/administration/administration.module.ts
@@ -6,9 +6,15 @@ import { LoginComponent } from "./login/login.component";
 import { SharedModule } from "../shared";
 import { AdminPageComponent } from "./admin-page";
 import { AdminHeaderComponent } from "./admin-header";
+import { AdminNewEventComponent } from "./admin-new-event";
 
 @NgModule({
-	declarations: [LoginComponent, AdminPageComponent, AdminHeaderComponent],
+	declarations: [
+		LoginComponent,
+		AdminPageComponent,
+		AdminHeaderComponent,
+		AdminNewEventComponent,
+	],
 	imports: [CommonModule, AdministrationRoutingModule, SharedModule],
 })
 export class AdministrationModule {}
