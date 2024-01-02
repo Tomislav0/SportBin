05559df04e659653e91ec7500158265ca502212a
lovrodijanovic <lovrodijanovic98@gmail.com>
2024-01-02 12:34:46 +0100

    Fixed issue with header dropdown

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule, FooterComponent, HeaderComponent } from './shared';
 import { HeadersInterceptor } from './helpers/headers.interceptor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
@@ -15,6 +16,7 @@ import { HeadersInterceptor } from './helpers/headers.interceptor';
     HttpClientModule,
     FormsModule,
     SharedModule,
+    NgbModule,
   ],
   providers: [
     {
