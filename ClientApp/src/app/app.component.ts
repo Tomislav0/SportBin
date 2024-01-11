55239e760c2d7151147bc35fbd8276ad98eb63b4
Zokky2e <icro4.gaming@hotmail.com>
2024-01-11 18:30:50 +0100

    tweeked styling and added scrolling to mobile version of event lists

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { AuthGuard } from './helpers/auth.guard';
+import { Component } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { AuthGuard } from "./helpers/auth.guard";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+	selector: "app-root",
+	templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  public isLogin: boolean = true;
-  public isAdmin = false;
-  constructor(private router: Router, private authGuard: AuthGuard) {
-    this.isAdmin = this.authGuard.isAdmin();
-    this.router.events.subscribe((event: any) => {
-      if (event instanceof NavigationEnd) {
-        if (event.url === '/admin/login') {
-          this.isLogin = true;
-        } else {
-          this.isLogin = false;
-        }
-      }
-    });
-  }
+	public isLogin: boolean = true;
+	public isAdmin = false;
+	constructor(private router: Router, private authGuard: AuthGuard) {
+		this.isAdmin = this.authGuard.isAdmin();
+		this.router.events.subscribe((event: any) => {
+			if (event instanceof NavigationEnd) {
+				if (event.url === "/admin/login") {
+					this.isLogin = true;
+				} else {
+					this.isLogin = false;
+				}
+			}
+		});
+	}
+
+	public changeAdminStatus(status: boolean) {
+		this.isAdmin = status;
+	}
 }
--- ClientApp/src/app/shared/layout/header/header.component.ts
+++ ClientApp/src/app/shared/layout/header/header.component.ts
@@ -1,63 +1,72 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { ICategoryDTO } from 'src/app/models';
-import { AuthService } from 'src/app/services/auth.service';
-import { CategoryService } from 'src/app/services/category.service';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { AuthGuard } from "src/app/helpers/auth.guard";
+import { ICategoryDTO } from "src/app/models";
+import { AuthService } from "src/app/services/auth.service";
+import { CategoryService } from "src/app/services/category.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+	selector: "app-header",
+	templateUrl: "./header.component.html",
+	styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent {
-  public isExpanded = false;
-  public isLoginPage: boolean = false;
-  public isAuthorized = false;
-  public categories: ICategoryDTO[] = [];
+	public isExpanded = false;
+	public isLoginPage: boolean = false;
+	public isAuthorized = false;
+	public categories: ICategoryDTO[] = [];
+	@Output() adminLogoutClicked: EventEmitter<boolean> =
+		new EventEmitter<boolean>();
 
-  constructor(
-    private categoryService: CategoryService,
-    private router: Router,
-    private authService: AuthService
-  ) {
-    this.isAuthorized = localStorage.getItem('auth_token') != null;
-    this.authService.authorizedSubject.subscribe((result) => {
-      this.isAuthorized = result;
-    });
-    this.router.events.subscribe((event: any) => {
-      if (event instanceof NavigationEnd) {
-        if (event.url === '/admin/login') {
-          this.isLoginPage = true;
-        } else {
-          this.isLoginPage = false;
-        }
-      }
-    });
+	constructor(
+		private categoryService: CategoryService,
+		private router: Router,
+		private authService: AuthService,
+		private authGuard: AuthGuard
+	) {
+		this.isAuthorized = localStorage.getItem("auth_token") != null;
+		this.authService.authorizedSubject.subscribe((result) => {
+			this.isAuthorized = result;
+		});
+		this.router.events.subscribe((event: any) => {
+			if (event instanceof NavigationEnd) {
+				if (event.url === "/admin/login") {
+					this.isLoginPage = true;
+				} else {
+					this.isLoginPage = false;
+				}
+			}
+		});
 
-    this.categoryService.getEventCategories().subscribe(
-      (result) => {
-        this.categories = result;
-      },
-      (error) => console.error(error)
-    );
-  }
+		this.categoryService.getEventCategories().subscribe(
+			(result) => {
+				this.categories = result;
+			},
+			(error) => console.error(error)
+		);
+	}
 
-  public onCategoryClicked(category: ICategoryDTO) {
-    this.router.navigate([`categoryEvents/${category.id}`]);
-  }
+	public onCategoryClicked(category: ICategoryDTO) {
+		this.router.navigate([`categoryEvents/${category.id}`]);
+	}
 
-  collapse() {
-    this.isExpanded = false;
-  }
+	public isAdminPage() {
+		return this.router.url.includes("admin");
+	}
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
-  }
+	collapse() {
+		this.isExpanded = false;
+	}
 
-  logout() {
-    localStorage.removeItem('auth_token');
-    this.isAuthorized = false;
-    this.authService.authorizedSubject.next(false);
-    window.location.reload();
-  }
+	toggle() {
+		this.isExpanded = !this.isExpanded;
+	}
+
+	logout() {
+		localStorage.removeItem("auth_token");
+		this.isAuthorized = false;
+		this.adminLogoutClicked.emit(false);
+		this.authService.authorizedSubject.next(false);
+		window.location.reload();
+	}
 }
